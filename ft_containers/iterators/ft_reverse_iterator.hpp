f1c085d133a920ea764dd046b80df8aa27390ea2
Cmerope <barinova.bms@gmail.com>
2022-11-14 21:27:00 +0300

    add: reverse_iterator

--- ft_containers/iterators/ft_reverse_iterator.hpp
+++ ft_containers/iterators/ft_reverse_iterator.hpp
@@ -9,8 +9,14 @@ namespace ft {
 	//  проверить - можно ли удалить ФТ внутри !!!
 	template< class Iter >
 	class reverse_iterator : public ft::iterator< typename ft::iterator_traits<Iter>::iterator_category,
-									typename ft::iterator_traits<Iter>::value_type, typename ft::iterator_traits<Iter>::difference_type, 
-									typename ft::iterator_traits<Iter>::pointer, typename ft::iterator_traits<Iter>::reference >{
+									typename ft::iterator_traits<Iter>::value_type,
+									typename ft::iterator_traits<Iter>::difference_type, 
+									typename ft::iterator_traits<Iter>::pointer,
+									typename ft::iterator_traits<Iter>::reference >{
+
+		protected:
+				iterator_type													current;
+
 		public:
 				typedef Iter													iterator_type;
 				typedef typename ft::iterator_traits<Iter>::iterator_category	iterator_category;
@@ -19,16 +25,40 @@ namespace ft {
 				typedef typename ft::iterator_traits<Iter>::pointer				pointer;
 				typedef typename ft::iterator_traits<Iter>::reference			reference;
 
-				reverse_iterator();
-				explicit reverse_iterator (iterator_type it);
+			// конструкторы
+				reverse_iterator(){}
+
+				explicit reverse_iterator (iterator_type it) : current(it){}
 
 				template <class Iter>
-				reverse_iterator (const reverse_iterator<Iter>& rev_it){};
-				iterator_type base() const{};
-				~reverse_iterator();
+				// rev_it.current или так забрать!
+				reverse_iterator (const reverse_iterator<Iter>& rev_it) : current(rev_it.base()){}
+			// деструкторы
+				~reverse_iterator(){}
+				
+			// 
+				template< class Iter > 
+				reverse_iterator& operator=( const reverse_iterator<Iter>& other ){
+					if (this == &other)
+						return (*this);
+					this->current = other.current;
+					return (*this);
+				}
 
-		protected:
-				iterator_type													current;
+				iterator_type base() const{
+					return(current);
+				}
+
+				reference operator*() const{
+					Iter tmp = this->current;
+    				return *--tmp;
+				}
+
+				// возврат адреса итератора
+				pointer operator->() const{
+					return (&(operator*()));
+				}
+				
 	};
 
 }
